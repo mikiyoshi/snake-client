d040aa91a4f2f887e2cadd940f21325944bcfb11
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-01 20:26:54 +0000

    this file update module exports

---
+++ client.js
@@ -0,0 +1,3 @@
+console.log('this is client.js ...');
+// const { connect } = require('./play');
+const { connect } = require('./play.js');
--- play.js
+++ play.js
@@ -4,14 +4,20 @@ const net = require('net');
 const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243', // IP address here,
-    port: '50541', // PORT number here,
+    port: 50541, // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
-
+  // conn.on('connect', () => {
+  //   console.log('Hello, this is Miki');
+  //   // code that does something when the connection is first established
+  // });
   return conn;
 };
 
 console.log('Connecting ...');
 connect();
+module.exports = {
+  connect,
+};
