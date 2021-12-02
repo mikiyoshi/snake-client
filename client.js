1f577f272c70564d3a408ded2d29750b283b3679
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-02 04:16:16 +0000

    this file update input.js

--- client.js
+++ client.js
@@ -1,9 +1,77 @@
-// console.log('this is client.js ...');
-// const { connect } = require('./play');
-const { connect } = require('./play.js');
+const net = require('net');
 
-// connect();
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: 50541, // PORT number here,
+  });
 
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    // conn.write('Name: K9R');
+    let arr = [
+      'Name: K9R',
+      'Move: up',
+      'Move: up',
+      'Move: left',
+      'Move: left',
+      'Move: up',
+      'Move: up',
+      'Move: left',
+      'Move: left',
+      'Move: up',
+      'Move: up',
+      'Move: left',
+      'Move: left',
+      'Move: up',
+      'Move: up',
+      'Move: left',
+      'Move: left',
+      'Move: down',
+      'Move: down',
+      'Move: left',
+      'Move: left',
+      'Move: down',
+      'Move: down',
+      'Move: left',
+      'Move: left',
+      'Move: down',
+      'Move: down',
+      'Move: left',
+      'Move: left',
+      'Move: down',
+      'Move: down',
+      'Move: left',
+      'Move: left',
+      'Move: down',
+      'Move: down',
+      'Move: left',
+      'Move: left',
+      'Move: down',
+      'Move: down',
+      'Move: left',
+      'Move: left',
+      'Move: down',
+      'Move: down',
+      'Move: left',
+      'Move: left',
+    ];
+    for (let i = 0; i < arr.length; i++) {
+      setTimeout(() => {
+        conn.write(arr[i]);
+      }, i * 100); // <= 1s delay to make it noticeable. Can dial it down later.
+    }
+  });
+  return conn;
+};
+
+console.log('Connecting ...');
+
+module.exports = {
+  connect,
+};
 // コールバック関数とは
 // function a(b) {
 //     b();
--- play.js
+++ play.js
@@ -1,93 +1,17 @@
-const net = require('net');
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541, // PORT number here,
-  });
-
-  const handleUserInput = function () {
-    // your code here
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-  const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-    stdin.on('data', handleUserInput);
-    return stdin;
-  };
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('connect', () => {
-    // conn.write('Name: K9R');
-    let arr = [
-      'Name: K9R',
-      'Move: up',
-      'Move: up',
-      'Move: left',
-      'Move: left',
-      'Move: up',
-      'Move: up',
-      'Move: left',
-      'Move: left',
-      'Move: up',
-      'Move: up',
-      'Move: left',
-      'Move: left',
-      'Move: up',
-      'Move: up',
-      'Move: left',
-      'Move: left',
-      'Move: down',
-      'Move: down',
-      'Move: left',
-      'Move: left',
-      'Move: down',
-      'Move: down',
-      'Move: left',
-      'Move: left',
-      'Move: down',
-      'Move: down',
-      'Move: left',
-      'Move: left',
-      'Move: down',
-      'Move: down',
-      'Move: left',
-      'Move: left',
-      'Move: down',
-      'Move: down',
-      'Move: left',
-      'Move: left',
-      'Move: down',
-      'Move: down',
-      'Move: left',
-      'Move: left',
-      'Move: down',
-      'Move: down',
-      'Move: left',
-      'Move: left',
-    ];
-    for (let i = 0; i < arr.length; i++) {
-      setTimeout(() => {
-        conn.write(arr[i]);
-      }, i * 100); // <= 1s delay to make it noticeable. Can dial it down later.
-    }
-  });
-  setupInput();
-  return conn;
-};
+// console.log('this is client.js ...');
+// const { connect } = require('./play');
+// const { connect } = require('./client.js');
+// const { setupInput } = require('./input.js');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
-module.exports = {
-  connect,
-};
+
+setupInput();
+
+// connect();
+
 // コールバック関数とは
 // function a(b) {
 //     b();
